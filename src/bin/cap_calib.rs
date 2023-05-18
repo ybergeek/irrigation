fcb0a3cb599a772e2ac592dae616d8b3fa5f486d
ybergeek <alf.jansson66@gmail.com>
2023-05-18 09:11:44 +0200

    add calibratior

---
+++ src/bin/cap_calib.rs
@@ -0,0 +1,20 @@
+use irrigation::read_sensors;
+use irrigation::voltage;
+use std::thread;
+use std::time::Duration;
+
+
+fn main() {
+    loop{
+        for (channel, value) in read_sensors().iter().enumerate() {
+            let int_value = i16::clone(value);
+            let voltage = voltage(int_value);
+            println!("Channel {}: value: {} voltage: {}", channel, value, voltage);
+            
+        }
+        // get I2C device back
+
+        thread::sleep(Duration::from_millis(500));
+    }
+      
+}
